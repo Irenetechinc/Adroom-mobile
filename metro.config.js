e8210f04c956ecd24c72c22e2719d05871fa9cad
irenetechinc <irenokpala617@gmail.com>
2026-01-29 15:07:57 +0100

    Fix EAS config and Metro config for Windows

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,5 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Use a relative path for input to avoid Windows absolute path issues with NativeWind v4
 module.exports = withNativeWind(config, { input: "./global.css" });
